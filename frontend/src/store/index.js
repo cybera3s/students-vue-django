90cd1d87d3fd7f4a22335a344ef9c0f3c0e5617a
ario <cybera.3s@gmail.com>
2022-05-16 23:02:03 +0430

    add onStart func to actions

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -25,6 +25,30 @@ export default createStore({
     },
   },
   actions: {
+      
+      /*
+      check if token exists in local storage then send get request to validate the
+      user with corresponding token if user exists ,login user again
+      else if corresponding token is invalid logout the user 
+      */
+      onStart(context) {
+        let token = localStorage.getItem('token')
+        if (token){
+           axios.defaults.headers.common['Authorization'] = "Token " + token
+
+          axios
+                .get('/api/auth/users/me/')
+                .then(response =>  {
+                      context.commit('login', token)
+
+                })
+                .catch(error => {
+                   context.commit('logout')
+
+                })
+
+        } 
+      },
   },
   modules: {
   }
