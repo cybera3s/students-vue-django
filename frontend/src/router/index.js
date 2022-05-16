6fecb292aac6f5a76c091d9dc968d2304e3167b3
ario <cybera.3s@gmail.com>
2022-05-16 22:35:53 +0430

    add route protecting

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import LogoutView from '../views/LogoutView.vue'
+import store from '../store'
 
 
 const routes = [
@@ -13,12 +14,15 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: LoginView
+    component: LoginView,
+    meta: { loginRedirect : true },
+
   },
   {
     path: '/logout',
     name: 'Logout',
-    component: LogoutView
+    component: LogoutView,
+    meta : { loginRequired: true }
   },
   
 ]
@@ -27,5 +31,24 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+
+// route protecting
+
+router.beforeEach((to, from) => {
+  
+  if (to.meta.loginRequired && !store.state.isAuthenticated) {
+    
+    return {
+      path: '/login'
+      // query: { redirect: to.fullPath },
+    }
+  } else if (to.meta.loginRedirect && store.state.isAuthenticated){
+    return {
+      path: '/'
+    }
+  }
+})
+
 
 export default router
