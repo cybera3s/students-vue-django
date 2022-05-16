8d874a74fba5d6b693f7cb9e97555caf43e39794
ario <cybera.3s@gmail.com>
2022-05-16 16:37:28 +0430

    add yasg urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, re_path, include
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework import permissions
 
 
 schema_view = get_schema_view(
@@ -24,4 +24,18 @@ urlpatterns = [
     # djoser urls
     path("api/auth/", include("djoser.urls")),
     path("api/auth/", include("djoser.urls.authtoken")),
+    # yasg urls
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
