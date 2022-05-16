7e18b50ecb7dd4f0882ee12abef19a3b27cb36df
ario <cybera.3s@gmail.com>
2022-05-16 16:37:47 +0430

    add Rest Framework Setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,3 +131,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Rest Framework Setting
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
+}
