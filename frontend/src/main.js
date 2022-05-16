355fa5bf712f4d6c089e94bb282a0f5e04c1d1b4
ario <cybera.3s@gmail.com>
2022-05-16 18:15:34 +0430

    add axios default base url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,4 +9,7 @@ import 'jquery/src/jquery.js'
 import 'popper.js/dist/popper.min.js'
 import "bootstrap/dist/js/bootstrap.min.js"
 
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
+
 createApp(App).use(store).use(router).mount('#app')
