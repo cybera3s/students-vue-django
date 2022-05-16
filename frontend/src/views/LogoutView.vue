1a3d95f5e171f48df747a63b0e25afe4dbced905
ario <cybera.3s@gmail.com>
2022-05-16 18:59:09 +0430

    add script for logout

---
+++ frontend/src/views/LogoutView.vue
@@ -0,0 +1,21 @@
+<script>
+	import axios from "axios"
+
+	export default {
+		name: "LogoutView",
+		beforeMount(){
+			axios
+              .post('/api/auth/token/logout/')
+              .then(response =>  {
+
+                    this.$store.commit("logout")
+                    this.$router.push("login")
+
+              })
+              .catch(error => {
+                  console.log(error.response.data)
+                  this.logoutError = error.response.data
+              })
+		},
+	}
+</script>
