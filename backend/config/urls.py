dfb48f9856f898a223c2fa916074a19511db72f5
ario <cybera.3s@gmail.com>
2022-05-16 16:01:41 +0430

    add djoser urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,4 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    # djoser urls
+    path("api/auth/", include("djoser.urls")),
+    path("api/auth/", include("djoser.urls.authtoken")),
 ]
