77d1ac569ce50b30da1acf23c4809cf58bed3e12
ario <cybera.3s@gmail.com>
2022-05-16 17:30:46 +0430

    del samples

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	
+	<div class="container">
+		<div class="alert alert-primary">
+			<h1>Home Page</h1>
+		</div>
+	</div>
+	
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
