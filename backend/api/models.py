f99b67315b628ca05994263405a5dfc6cb4da885
ario <cybera.3s@gmail.com>
2022-05-16 16:42:11 +0430

    add __str__ method > Student model

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,3 +5,6 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     field = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
