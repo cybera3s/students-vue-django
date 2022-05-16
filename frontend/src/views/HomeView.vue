173430f7062ff8e9e809c08c73d9c005c0f53777
ario <cybera.3s@gmail.com>
2022-05-16 18:41:07 +0430

    add paragraph descrption

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,7 +3,8 @@
 	<div class="container">
 		<div class="alert alert-primary">
 			<h1>Home Page</h1>
+			<p style="text-transform: capitalize;">students sample project using vueJs3 and django rest framework</p>
 		</div>
 	</div>
-	
+
 </template>
