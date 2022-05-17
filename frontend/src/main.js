770f68d92b7b264b1e04dc4829b9950cd7247be8
ario <cybera.3s@gmail.com>
2022-05-17 19:43:31 +0430

    import and use sweetalert2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/src/jquery.js'
@@ -12,4 +14,4 @@ import "bootstrap/dist/js/bootstrap.min.js"
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(VueSweetalert2).mount('#app')
