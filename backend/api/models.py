e401acbb3413833962b5646182f761056083ea17
ario <cybera.3s@gmail.com>
2022-05-22 20:14:46 +0430

    refactor max_length of fields

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=30)
     age = models.IntegerField()
-    field = models.CharField(max_length=100)
+    field = models.CharField(max_length=35)
 
     def __str__(self):
         return self.name
